6a2390bb026c75e51e47605133fa18e590fa958e
GabrielSun77 <itjustforclaude@gmail.com>
2026-09-22 17:45:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+/* 儲備管家 service worker：讓 App 離線也能開 */
+const CACHE = 'pantry-keeper-v0.1.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 網路優先、失敗才用快取：這樣更新到 GitHub Pages 後重開 App 就會拿到新版
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
